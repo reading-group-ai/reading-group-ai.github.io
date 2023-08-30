86866c7b22392b31b2478ebc129106341e3ac4ea
reading-group-ai <harsh.prodromus@gmail.com>
2023-08-30 21:06:33 +0400

    added csv7

--- scripts.js
+++ scripts.js
@@ -6,14 +6,15 @@ document.addEventListener("DOMContentLoaded", function() {
         let htmlContent = '';
 
         rows.forEach(row => {
-            const [presenter, title, link, year, source] = row.split(',');
+            const [presenter, title, linkp, year, source,linkm] = row.split(',');
             htmlContent += `
                 <tr>
                     <td>${presenter}</td>
                     <td>${title}</td>
-                    <td><a href="${link}" target="_blank">Link</a></td>
+                    <td><a href="${linkp}" target="_blank">Link paper</a></td>
                     <td>${year}</td>
                     <td>${source}</td>
+                    td><a href="${linkm}" target="_blank">Link meeting</a></td>
                 </tr>
             `;
         });
