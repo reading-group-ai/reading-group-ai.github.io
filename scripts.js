d16ebd62311560d4dff7c5e9e12c57fbc46de325
reading-group-ai <harsh.prodromus@gmail.com>
2023-08-30 19:21:53 +0400

    added csv2

--- scripts.js
+++ scripts.js
@@ -1,30 +1,32 @@
+// Fetch the CSV data
 fetch('papers.csv')
     .then(response => response.text())
     .then(data => {
         const rows = data.split('\n');
-        let tableContent = `<thead>
-            <tr>
-                <th>Presenter Name</th>
-                <th>Paper Title</th>
-                <th>Link</th>
-                <th>Year</th>
-                <th>Source</th>
-            </tr>
-        </thead>
-        <tbody>`;
-        
-        for(let i = 1; i < rows.length; i++) {
-            const columns = rows[i].split(',');
-            tableContent += '<tr>';
-            columns.forEach(column => {
-                if (columns.indexOf(column) == 2) {
-                    tableContent += `<td><a href="${column.trim()}" target="_blank">Link</a></td>`;
+        const headers = rows[0].split(', ');
+
+        const table = document.getElementById('papersTable').getElementsByTagName('tbody')[0];
+
+        // Start from 1 to skip the header row
+        for (let i = 1; i < rows.length; i++) {
+            const cells = rows[i].split(', ');
+            const row = table.insertRow(-1);  // Insert a new row at the last position
+
+            for (let j = 0; j < cells.length; j++) {
+                const cell = row.insertCell(j);
+                if (j === 2) {
+                    // Create a link for the paper
+                    const link = document.createElement('a');
+                    link.href = cells[j];
+                    link.innerText = "View Paper";
+                    link.target = "_blank";
+                    cell.appendChild(link);
                 } else {
-                    tableContent += `<td>${column.trim()}</td>`;
+                    cell.innerText = cells[j];
                 }
-            });
-            tableContent += '</tr>';
+            }
         }
-        tableContent += '</tbody>';
-        document.getElementById('papersTable').innerHTML = tableContent;
+    })
+    .catch(error => {
+        console.error('Error fetching the CSV data:', error);
     });
